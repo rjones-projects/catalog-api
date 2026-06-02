8edeea21a68c54c9de4763d7d401e6f2397c1c89
rjones-projects <roy.jones@microservicesolutions.com>
2026-06-02 10:17:03 +0100

    updated catalog-api to use gcp provider >=7.17

--- app/catalog_resolver.py
+++ app/catalog_resolver.py
@@ -330,7 +330,7 @@ class CatalogResolver:
             "  required_providers {",
             "    google = {",
             '      source  = "hashicorp/google"',
-            '      version = "~> 5.0"',
+            '      version = ">= 7.17.0"',
             "    }",
             "  }",
         ]
